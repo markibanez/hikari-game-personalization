ce7674dae1ec5574796574a0bbc353781ee62958
Mark Ibanez <mark.ibanez@gmail.com>
2022-02-28 11:16:16 +0800

    Customized app level UI

--- web-app/pages/index.js
+++ web-app/pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
             <Head>
                 <title>Hikari</title>
                 <meta name="description" content="Hikari Game Personalization" />
-                <link rel="icon" href="/favicon.ico" />
+                <link rel="icon" href="/images/logo.jpg" />
             </Head>
 
             <main className={styles.main}>

ba5d03a8821bbcb814b4c774170698a7ab51583b
Mark Ibanez <mark.ibanez@gmail.com>
2022-02-28 13:42:32 +0800

    Update index.js

--- web-app/pages/index.js
+++ web-app/pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import Image from 'next/image';
+import ConnectWallet from '../components/ConnectWallet';
 import styles from '../styles/Home.module.css';
 import data from './data.json';
 
@@ -11,10 +12,14 @@ export default function Home() {
                 <title>Hikari</title>
                 <meta name="description" content="Hikari Game Personalization" />
                 <link rel="icon" href="/images/logo.jpg" />
+                <link
+                    rel="stylesheet"
+                    href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+                />
             </Head>
 
             <main className={styles.main}>
-
+                <ConnectWallet />
             </main>
         </div>
     );

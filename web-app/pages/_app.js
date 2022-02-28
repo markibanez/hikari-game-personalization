c0c385d82f3f8500a9ffc14535c847631b5a43f5
Mark Ibanez <mark.ibanez@gmail.com>
2022-02-28 11:41:58 +0800

    Added providers

--- web-app/pages/_app.js
+++ web-app/pages/_app.js
@@ -1,7 +1,21 @@
+import { SnackbarProvider } from 'notistack';
+import { WalletProvider } from '../contexts/WalletContext';
 import './styles.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <SnackbarProvider
+            maxSnack={6}
+            anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'right'
+            }}
+        >
+            <WalletProvider>
+                <Component {...pageProps} />
+            </WalletProvider>
+        </SnackbarProvider>
+    );
 }
 
-export default MyApp
+export default MyApp;

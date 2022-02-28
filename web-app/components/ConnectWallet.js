3b0e278f37630435ca24bc2f3703781ffb53c3ed
Mark Ibanez <mark.ibanez@gmail.com>
2022-02-28 14:53:17 +0800

    Added fade transition

--- web-app/components/ConnectWallet.js
+++ web-app/components/ConnectWallet.js
@@ -1,25 +1,32 @@
-import { Button, Card, Stack, Typography } from '@mui/material'
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { Button, Card, Fade, Stack, Typography } from '@mui/material'
+import { ethers } from 'ethers';
 import { WalletContext } from '../contexts/WalletContext'
 
 export default function ConnectWallet(props) {
     const wallet = useContext(WalletContext);
+    const [visible, setVisible] = useState(true);
+
+    useEffect(() => {
+        setVisible(wallet.address ? false : true);
+    }, [wallet.address]);
 
     return (
         <>
-            <Card
-                sx={{
-                    padding: 4,
-                    opacity: 0.8
-                }}
-            >
-                <Stack>
-                    <Typography variant="h4" sx={{ marginBottom: 3 }}>
-                        connect your web3 wallet
-                    </Typography>
-                    <Button variant="outlined" size="large" onClick={wallet.connect}>Connect</Button>
-                </Stack>
-            </Card>
+            <Fade in={visible} timeout={1000}>
+                <Card
+                    sx={{
+                        padding: 4,
+                    }}
+                >
+                    <Stack>
+                        <Typography variant="h4" sx={{ marginBottom: 3 }}>
+                            connect your web3 wallet
+                        </Typography>
+                        <Button variant="outlined" size="large" onClick={wallet.connect}>Connect</Button>
+                    </Stack>
+                </Card>
+            </Fade>
         </>
     )
 }

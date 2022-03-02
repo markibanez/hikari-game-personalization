8dd162c80b8ef8443e33d2af8da2607d612fb043
Mark Ibanez <mark.ibanez@gmail.com>
2022-03-02 22:28:10 +0800

    Refactored to _document.js

--- web-app/pages/index.js
+++ web-app/pages/index.js
@@ -1,25 +1,12 @@
-import Head from 'next/head';
-import Image from 'next/image';
 import ConnectWallet from '../components/ConnectWallet';
 import SelectToken from '../components/SelectToken';
 import WalletInfo from '../components/WalletInfo';
 import styles from '../styles/Home.module.css';
-import data from './data.json';
 
 export default function Home() {
 
     return (
         <div className={styles.container}>
-            <Head>
-                <title>Hikari</title>
-                <meta name="description" content="Hikari Game Personalization" />
-                <link rel="icon" href="/images/logo.jpg" />
-                <link
-                    rel="stylesheet"
-                    href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-                />
-            </Head>
-
             <main className={styles.main}>
                 <WalletInfo />
                 <ConnectWallet />

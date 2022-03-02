dbaea08d128782add74ebd1ba429019e93d927ab
Mark Ibanez <mark.ibanez@gmail.com>
2022-03-02 22:27:54 +0800

    Create _document.js

---
+++ web-app/pages/_document.js
@@ -0,0 +1,21 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+    return (
+        <Html>
+            <Head>
+                <title>Hikari</title>
+                <meta name="description" content="Hikari Game Personalization" />
+                <link rel="icon" href="/images/logo.jpg" />
+                <link
+                    rel="stylesheet"
+                    href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+                />
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
+}

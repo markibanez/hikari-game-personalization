21ff556df676ff5382aac438baf7faadecbb186f
Mark Ibanez <mark.ibanez@gmail.com>
2022-03-29 16:16:18 +0800

    Updated drop dead gorgeous web font

--- web-app/pages/_document.js
+++ web-app/pages/_document.js
@@ -7,6 +7,7 @@ export default function Document() {
                 <title>Hikari</title>
                 <meta name="description" content="Hikari Game Personalization" />
                 <link rel="icon" href="/images/logo.jpg" />
+                <link rel="stylesheet" href="/MyFontsWebfontsKit.css" />
                 <link
                     rel="stylesheet"
                     href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"

1dad0631bcf9e1eeb54f4f01347dabaf1076f527
Mark Ibanez <mark.ibanez@gmail.com>
2022-04-07 11:18:34 +0800

    Go back button and text case fix

--- web-app/components/Start.js
+++ web-app/components/Start.js
@@ -3,12 +3,16 @@ import LoadingButton from '@mui/lab/LoadingButton';
 import { useContext, useState } from 'react';
 import { WalletContext } from '/contexts/WalletContext';
 import { useSnackbar } from 'notistack';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 export default function Start(props) {
     const { setState, setDecision, address, token } = props;
     const wallet = useContext(WalletContext);
     const [verifying, setVerifying] = useState(false);
     const { enqueueSnackbar } = useSnackbar();
+
+    const router = useRouter();
 
     const sign = async () => {
         setVerifying(true);
@@ -19,7 +23,8 @@ export default function Start(props) {
 
             const response = await fetch(`/api/update-state?address=${address}&token=${token}&signature=${signature}`);
             if (response.status === 200) {
-                const result = await response.json(); console.log(result);
+                const result = await response.json();
+                console.log(result);
                 setState(result.state);
                 setDecision(result.decision);
             } else {
@@ -36,30 +41,43 @@ export default function Start(props) {
     return (
         <>
             <Box
-                    sx={{
-                        width: '800px',
-                        height: '600px',
-                        textAlign: 'center',
-                        backgroundImage: `url("/images/branded-modal-2.png")`,
-                        backgroundSize: 'contain',
-                        backgroundRepeat: 'no-repeat',
-                        paddingTop: '220px',
-                        marginTop: '140px'
-                    }}
+                sx={{ position: 'absolute', top: '78%', left: '50%', transform: `translate(-50%, -55%)` }}
+                onClick={() => router.back()}
+            >
+                <Typography variant="h5" sx={{ padding: '15px 25px', fontWeight: 700, color: '#302C21' }}>
+                    Back to Home
+                </Typography>
+            </Box>
+            <Box
+                sx={{
+                    width: '56%',
+                    height: '56%',
+                    textAlign: 'center',
+                    backgroundImage: `url("/images/branded-modal-2.png")`,
+                    backgroundSize: 'contain',
+                    backgroundRepeat: 'no-repeat',
+                    backgroundPosition: 'center',
+                    // paddingTop: '180px',
+                    position: 'absolute',
+                    top: '50%',
+                    left: '50%',
+                    transform: `translate(-50%, -55%)`,
+                }}
+            >
+                <Typography
+                    variant="h3"
+                    sx={{ fontFamily: 'DK-DDG', color: '#AEAD8F', marginTop: '21vmin', fontSize: '5vmin',textShadow: '2px 2px #413D31', }}
                 >
-
-                    <Typography variant="h3" sx={{ fontFamily: 'DK-DDG', color: '#AEAD8F', marginTop: '10px' }}>
-                        SIGN TRANSACTION
-                    </Typography>
-                    <Typography variant="h4" sx={{ paddingX: 10, fontSize: '20pt', color: '#302C21', marginTop: 2 }}>
-                        You must approve a state change in your wallet to begin the Gen-U experience with this soul.
-                    </Typography>
-                    <br/>
-                    <img
-                        src="/images/sign-button.png"
-                        style={{ width: 300, marginTop: 10 }}
-                        onClick={sign}
-                    />
+                    SIGN TRANSACTION
+                </Typography>
+                <Typography
+                    variant="h4"
+                    sx={{ paddingX: '20%', fontSize: '20pt', color: '#302C21', marginTop: 2, fontSize: '3vmin' }}
+                >
+                    You must approve a state change in your wallet to begin the Gen-U experience with this soul.
+                </Typography>
+                <br />
+                <img src="/images/sign-button.png" style={{ width: '20%', marginTop: '0' }} onClick={sign} />
             </Box>
             {/* <Card>
                 <CardContent>
